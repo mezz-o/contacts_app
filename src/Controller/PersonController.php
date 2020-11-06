174c6c3c53aede54edad25f517cd41920fa050f7
mezz-o <mezzo.dev@gmail.com>
2020-11-06 17:43:09 +0100

    add create and edit functions in Person Controller

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -2,16 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Person;
+use App\Form\PersonType;
 use App\Repository\PersonRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PersonController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/", name="app_home", methods="GET")
      */
     public function index(PersonRepository $personRepository): Response
     {
@@ -22,6 +25,46 @@ class PersonController extends AbstractController
         return $this->render('person/index.html.twig', compact('persons'));
     }
 
-    
+    /**
+     * @Route("/create", name="app_person_create", methods="GET|POST")
+     */
+    public function create(EntityManagerInterface $em, Request $req): Response
+    {
+
+        $person = new Person;
+        $form = $this->createForm(PersonType::class, $person);
+        $form->handleRequest($req);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em->persist($person);
+            $em->flush();
+
+            $this->addFlash("succes", "New person successfuly added!");
+            return $this->redirectToRoute("app_home");
+        }
+
+        return $this->render("person/create.html.twig", ["form"=>$form->createView()]);
+
+    }
+
+    /**
+     * @Route("/edit/{id<[0-9]+>}", name="app_person_edit", methods="GET|POST")
+     */
+    public function edit(Person $person, EntityManagerInterface $em, Request $req): Response
+    {
+
+        $form = $this->createForm(PersonType::class, $person);
+        $form->handleRequest($req);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em->flush();
+            return $this->redirectToRoute("app_home");
+        }
+
+        return $this->render("/person/edit.html.twig", ["form"=>$form->createView()]);
+
+    }
 
 }
